5994ccd143857d06098eba3269fbf39a4b616876
fimmind <fimmind@mail.ru>
2020-09-02 21:50:32 +0300

    Add borders

--- src/game/field.rs
+++ src/game/field.rs
@@ -1,5 +1,7 @@
 use ndarray::Array2;
 use std::io::{stdout, Stdout, Write};
+use std::iter;
+use termion::clear;
 use termion::color::{self, Color, Fg};
 use termion::cursor;
 use termion::raw::{IntoRawMode, RawTerminal};
@@ -25,10 +27,19 @@ impl Field {
 
     pub fn show(&mut self) {
         self.shown = true;
+
+        write!(
+            self.term,
+            "{}{}{}",
+            ToAlternateScreen,
+            clear::All,
+            cursor::Hide
+        )
+        .unwrap();
+        self.term.flush().unwrap();
         self.term.activate_raw_mode().unwrap();
 
-        write!(self.term, "{}{}", ToAlternateScreen, cursor::Hide).unwrap();
-        self.term.flush().unwrap();
+        self.draw_border();
         for ((x, y), s) in self.cells.indexed_iter() {
             print_at_cell(&mut self.term, (x, y), s);
         }
@@ -39,6 +50,38 @@ impl Field {
         write!(self.term, "{}{}", ToMainScreen, cursor::Show).unwrap();
         self.term.flush().unwrap();
         self.term.suspend_raw_mode().unwrap();
+    }
+
+    pub fn draw_border(&mut self) {
+        let (size_x, size_y) = self.cells.dim();
+        write!(self.term, "{}", Fg(color::White)).unwrap();
+
+        write!(
+            self.term,
+            "{}┏{}┓",
+            cursor::Goto(1, 1),
+            iter::repeat('━').take(size_x * 2 + 1).collect::<String>(),
+        )
+        .unwrap();
+
+        for i in 1..=size_y as u16 {
+            write!(
+                self.term,
+                "{}┃{}┃",
+                cursor::Goto(1, i + 1),
+                cursor::Goto(size_x as u16 * 2 + 3, i + 1),
+            )
+            .unwrap();
+        }
+
+        write!(
+            self.term,
+            "{}┗{}┛",
+            cursor::Goto(1, size_y as u16 + 2),
+            iter::repeat('━').take(size_x * 2 + 1).collect::<String>(),
+        ).unwrap();
+
+        write!(self.term, "{}", Fg(color::Reset)).unwrap();
     }
 
     pub fn set_cell(&mut self, cell: (usize, usize), color: impl Color) {
@@ -60,6 +103,6 @@ impl Field {
 fn print_at_cell<S: Write>(screen: &mut S, (x, y): (usize, usize), cell_value: &str) {
     let x = x as u16;
     let y = y as u16;
-    write!(screen, "{}{}", cursor::Goto(x * 2 + 1, y + 1), cell_value,).unwrap();
+    write!(screen, "{}{}", cursor::Goto(x * 2 + 3, y + 2), cell_value,).unwrap();
     screen.flush().unwrap();
 }
