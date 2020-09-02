39cc49c22a2d7ab821af3823caac5d04ab83eb88
fimmind <fimmind@mail.ru>
2020-09-02 17:55:34 +0300

    Make help message more informative

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
 mod game;
 
 use game::Game;
-use clap::{App, Arg};
+use clap::{App, Arg,};
 
 fn main() {
     let matches = App::new("snake")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
-        .about("Just a simple snake game")
+        .about("A simple snake game. You can use both arrows and vim-like keys")
         .args(&[
             Arg::with_name("field width")
-                .help("Witdth of the game field")
+                .help("Field's width (cells)")
                 .long("width")
                 .short("w")
                 .value_name("N")
                 .default_value("20"),
             Arg::with_name("field height")
-                .help("Height of the game field")
+                .help("Field's height (cells)")
                 .long("height")
                 .short("h")
                 .value_name("N")
                 .default_value("10"),
             Arg::with_name("step delay")
-                .help("Delay before a next step")
+                .help("Step delay (miliseconds)")
                 .long("delay")
                 .short("d")
                 .takes_value(true)
-                .value_name("MILISECONDS")
-                .default_value("200")
+                .value_name("N")
+                .default_value("285")
         ])
         .get_matches();
 
