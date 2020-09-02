cb0bb8981e0c84eebd947d0c281ea89bfe03c019
fimmind <fimmind@mail.ru>
2020-09-02 16:31:07 +0300

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
 mod game;
 
 use game::Game;
+use clap::{App, Arg};
 
 fn main() {
-    Game::new((30, 20)).start(300);
+    let matches = App::new("snake")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("Just a simple snake game")
+        .args(&[
+            Arg::with_name("field width")
+                .help("Witdth of the game field")
+                .long("width")
+                .short("w")
+                .value_name("N")
+                .default_value("20"),
+            Arg::with_name("field height")
+                .help("Height of the game field")
+                .long("height")
+                .short("h")
+                .value_name("N")
+                .default_value("10"),
+            Arg::with_name("step delay")
+                .help("Delay before a next step")
+                .long("delay")
+                .short("d")
+                .takes_value(true)
+                .value_name("MILISECONDS")
+                .default_value("200")
+        ])
+        .get_matches();
+
+    let width = matches.value_of("field width").unwrap().parse().unwrap();
+    let height = matches.value_of("field height").unwrap().parse().unwrap();
+    let delay = matches.value_of("step delay").unwrap().parse().unwrap();
+
+    Game::new((width, height)).start(delay);
 }
